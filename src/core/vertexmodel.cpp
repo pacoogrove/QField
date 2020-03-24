40e6b8eb1c307927a2dfaa06a26e2f2177d1f924
Matthias Kuhn <matthias@opengis.ch>
2020-03-24 21:37:24 +0100

    Only recenter canvas when current vertex index changes
    And not on every change of the position of the current vertex.
    This is especially important for digitizing with a pen because the current vertex can be anywhere on the screen.

--- src/core/vertexmodel.cpp
+++ src/core/vertexmodel.cpp
@@ -125,6 +125,7 @@ void VertexModel::refreshGeometry()
 
   setDirty( false );
 
+  emit currentVertexIndexChanged();
   emit vertexCountChanged();
 
   // for points, enable the editing mode directly
@@ -309,6 +310,7 @@ void VertexModel::setCurrentVertex( int newVertex, bool forceUpdate )
 
   int oldVertex = mCurrentIndex;
   mCurrentIndex = newVertex;
+  emit currentVertexIndexChanged();
 
   if ( mMode == AddVertex && oldVertex >= 0 )
   {
@@ -405,6 +407,20 @@ VertexModel::Centroid VertexModel::segmentCentroid( int leftIndex, int rightInde
   }
 
   return centroid;
+}
+
+void VertexModel::setCurrentVertexIndex( int currentIndex )
+{
+  if ( currentIndex == mCurrentIndex )
+    return;
+
+  mCurrentIndex = currentIndex;
+  emit currentVertexIndexChanged();
+}
+
+int VertexModel::currentVertexIndex() const
+{
+  return mCurrentIndex;
 }
 
 int VertexModel::vertexCount() const
